1a458d80def9bc08b463c5b79a9334de833d0a75
Sven Heyll <sven.heyll@lindenbaum.eu>
2016-07-19 13:04:15 +0200

    Rename Ftyp -> FileType

---
+++ src/Data/ByteString/IsoBaseFileFormat/Boxes/FileType.hs
@@ -0,0 +1,22 @@
+module Data.ByteString.IsoBaseFileFormat.Boxes.FileType where
+
+import Data.ByteString.IsoBaseFileFormat.Boxes.Box
+
+-- | File Type Box
+type FileTypeBox = Box FileType
+
+-- | Create a 'FileTypeBox' from a major brand, a minor version and a list of
+-- compatible brands
+ftypBox :: FileType -> FileTypeBox
+ftypBox = box "ftyp"
+
+-- | Contents of a 'ftyp' box are some 'FourCc' /brands/ and a version.
+data FileType =
+  FileType {majorBrand :: FourCc
+           ,minorVersion :: Word32
+           ,compatibleBrands :: [FourCc]}
+
+instance IsBoxContent FileType where
+  boxSize (FileType maj ver comps) = boxSize maj + 4 + sum (boxSize <$> comps)
+  boxBuilder (FileType maj ver comps) =
+    boxBuilder maj <> word32BE ver <> mconcat (boxBuilder <$> comps)
