1cded3d0ae174dbe4abd6010d030415d840e9515
Sven Heyll <sven.heyll@lindenbaum.eu>
2016-07-19 05:12:04 +0200

    WIP Implement boxBuilder BoxTypeExtension

--- src/Media/IsoBaseFileFormat/Blaze/Types/Box.hs
+++ src/Media/IsoBaseFileFormat/Blaze/Types/Box.hs
@@ -10,11 +10,8 @@ import Data.Word
 import GHC.TypeLits
 
 -- * Common boxes
-
 -- xxx = showBox (Proxy :: Proxy SvensBox) (Extend (FullBoxHeader undefined undefined) (Svens "hello"))
-
 mkSvensBox :: Word64 -> SvensBox
-
 mkSvensBox str =
   Box (FullBoxHeader (BoxVersion 3)
                      (BoxFlags 0) <|
@@ -24,26 +21,27 @@ type SvensBox = Box "sven"
 
 instance IsBox "sven" where
   type BoxContent "sven" = FullBox Svens
-  boxType _ = FourCc ('s', 'v', 'e', 'n')
+  boxType _ = FourCc ('s','v','e','n')
 
-data Svens = Svens Word64
+data Svens =
+  Svens Word64
 
 instance IsBoxContent Svens where
   boxSize (Svens s) = BoxSize s
   boxBuilder (Svens s) = word64BE s
 
 -- * Abstract Boxes
-
 -- | A `Box` with /version/ and /branding/ information
 type FullBox t = Extend FullBoxHeader t
 
 -- | The additional header with /version/ and /branding/ information
-data FullBoxHeader = FullBoxHeader BoxVersion (BoxFlags 24)
+data FullBoxHeader =
+  FullBoxHeader BoxVersion
+                (BoxFlags 24)
 
 instance IsBoxContent FullBoxHeader where
   boxSize (FullBoxHeader _ f) = 4 + boxSize f
-  boxBuilder (FullBoxHeader (BoxVersion v) f) =
-      word8 v <> boxBuilder f
+  boxBuilder (FullBoxHeader (BoxVersion v) f) = word8 v <> boxBuilder f
 
 -- | The box version (in a 'FullBox') is a single byte
 newtype BoxVersion =
@@ -57,11 +55,13 @@ newtype BoxFlags bits =
 
 -- | Internal function that creates a bit mask with all bits in a 'BoxFlags' set
 -- to 1.
-boxFlagBitMask :: KnownNat bits => BoxFlags bits -> Integer
+boxFlagBitMask :: KnownNat bits
+               => BoxFlags bits -> Integer
 boxFlagBitMask px = 2 ^ natVal px - 1
 
 -- | Internal function that masks-out all bits higher than 'bits'.
-cropBits :: KnownNat bits => BoxFlags bits -> BoxFlags bits
+cropBits :: KnownNat bits
+         => BoxFlags bits -> BoxFlags bits
 cropBits f@(BoxFlags b) = BoxFlags (b .&. boxFlagBitMask f)
 
 -- | Get the number of bytes required to store a number of bits.
@@ -71,7 +71,9 @@ instance KnownNat bits => IsBoxContent (BoxFlags bits) where
         modBytes = fromInteger $ natVal f `mod` 8
     in BoxSize $ minBytes + signum modBytes
   boxBuilder f@(BoxFlags b) =
-    let bytes = let (BoxSize bytes') = boxSize f in fromIntegral bytes'
+    let bytes =
+          let (BoxSize bytes') = boxSize f
+          in fromIntegral bytes'
         wordSeq n
           | n <= bytes =
             word8 (fromIntegral (shiftR b ((bytes - n) * 8) .&. 255)) <>
@@ -100,10 +102,9 @@ instance KnownNat bits => Bits (BoxFlags bits) where
   zeroBits = BoxFlags 0
 
 -- * /The/ actual Box
-
 -- | A type that wraps the contents of a box.
 data Box t where
-  Box :: IsBox b => BoxContent b -> Box b
+        Box :: IsBox b => BoxContent b -> Box b
 
 instance (IsBox t) => IsBox (Box t) where
   type BoxContent (Box t) = Box (BoxContent t)
@@ -123,7 +124,6 @@ instance IsBoxContent t => IsBoxContent (Box t) where
           t = boxType b
 
 -- * Box Meta Data
-
 -- | The box header contains a size and a type. Both can be compact (32bit) or
 -- large (64bit size, 17*32bit type).
 data BoxHeader =
@@ -135,8 +135,10 @@ data BoxHeader =
 -- provided as a 'Word64' which will be represented as either a 32bit compact
 -- size or as 64 bit /largesize/. If 'UnlimitedSize' is used, the box extends to
 -- the end of the file.
-data BoxSize = UnlimitedSize | BoxSize Word64
-  deriving (Show, Eq)
+data BoxSize
+  = UnlimitedSize
+  | BoxSize Word64
+  deriving (Show,Eq)
 
 instance IsBoxContent BoxSize where
   boxSize _ = BoxSize 4
@@ -166,7 +168,8 @@ instance Num BoxSize where
 
 -- | The 'BoxSize' can be > 2^32 in which case an 'BoxSizeExtension' must be
 -- added after the type field.
-data BoxSizeExtension = BoxSizeExtension BoxSize
+data BoxSizeExtension =
+  BoxSizeExtension BoxSize
 
 instance IsBoxContent BoxSizeExtension where
   boxBuilder (BoxSizeExtension UnlimitedSize) = mempty
@@ -181,48 +184,53 @@ instance IsBoxContent BoxSizeExtension where
        then 0
        else 8
 
-
 -- | A box has a /type/, this is the value level representation for the box type.
-data BoxType =
-  -- | `FourCc` can be used as @boxType@ in `Box`, standard four letter character
-  -- code, e.g. @ftyp@
-  FourCc (Char,Char,Char,Char)
-  -- | CustomBoxType defines custom @boxType@s in `Box`es.
-  | CustomBoxType UUID
-  deriving (Show, Eq)
+data BoxType
+  =
+    -- | `FourCc` can be used as @boxType@ in `Box`, standard four letter character
+    -- code, e.g. @ftyp@
+    FourCc (Char,Char,Char,Char)
+  |
+    -- | CustomBoxType defines custom @boxType@s in `Box`es.
+    CustomBoxType UUID
+  deriving (Show,Eq)
 
 newtype UUID =
   UUID String -- TODO
-  deriving (Show, Eq)
+  deriving (Show,Eq)
 
 instance IsBoxContent BoxType where
   boxSize _ = 4
   boxBuilder t =
-                  case t of
-                    FourCc x -> putFourCc x
-                    CustomBoxType (UUID u) ->
-                        putFourCc ('u','u','i','d')
-             where
-                putFourCc (a,b,c,d) = putW a <> putW b <> putW c <> putW d
-                putW = word8 . fromIntegral . fromEnum
+    case t of
+      FourCc x -> putFourCc x
+      CustomBoxType (UUID u) -> putFourCc ('u','u','i','d')
+    where putFourCc (a,b,c,d) = putW a <> putW b <> putW c <> putW d
+          putW = word8 . fromIntegral . fromEnum
 
 -- | When using custom types extra data must be written after the extra size
 -- information. Since the box type and the optional custom box type are not
 -- guaranteed to be consequtive, this type handles the /second/ part seperately.
-data BoxTypeExtension = BoxTypeExtension BoxType
+data BoxTypeExtension =
+  BoxTypeExtension BoxType
 
 instance IsBoxContent BoxTypeExtension where
   boxSize (BoxTypeExtension (FourCc _)) = 0
   boxSize (BoxTypeExtension (CustomBoxType _)) = 16 * 4
-
+  boxBuilder (BoxTypeExtension (FourCc _)) = mempty
+  boxBuilder (BoxTypeExtension (CustomBoxType (UUID str))) =
+    mconcat (map (word8 . fromIntegral . fromEnum)
+                 (take (16 * 4) str) ++
+             repeat (word8 0))
 
 -- * Box inheritance/extension
-
 -- | A meta box that mimic object oriented programming inheritance. The first
 -- parameter is the /base class/ and should be instance of 'IsBoxContent' and
 -- the second parameter is the /derived class/. This can represent
 -- 'IsBoxContent' as well as 'IsBox' instances.
-data Extend a b = Extend a b
+data Extend a b =
+  Extend a
+         b
 
 -- | A nice operator to for 'Extend'.
 (<|) :: a -> b -> Extend a b
@@ -230,19 +238,19 @@ data Extend a b = Extend a b
 
 instance (IsBoxContent p,IsBoxContent c) => IsBoxContent (Extend p c) where
   boxSize (Extend p c) = boxSize p + boxSize c
+  boxBuilder (Extend p c) = boxBuilder p <> boxBuilder c
 
 -- | While 'Extend' is an instance of 'IsBoxContent' the (uninhabited) promoted
 -- type has an 'IsBox' instance with the content type set to the unpromoted
 -- 'Extend'.
-instance (IsBoxContent parent, IsBox child) => IsBox ('Extend parent child) where
+instance (IsBoxContent parent,IsBox child) => IsBox ('Extend parent child) where
   type BoxContent ('Extend parent child) = Extend parent (BoxContent child)
   boxType _ = boxType (Proxy :: Proxy child)
 
 -- * Box type classes and functions
-
 -- | Types that are boxes. A box has a box type which is
 -- identified on the type level, hence the use of the proxy.
-class IsBoxContent (BoxContent a) => IsBox a where
+class IsBoxContent (BoxContent a) => IsBox a  where
   type BoxContent a
   -- | Type-level /box types/ can be converted into value level box types.
   boxType :: proxy a -> BoxType
@@ -250,6 +258,6 @@ class IsBoxContent (BoxContent a) => IsBox a where
 -- | Types that go into a box. A box content is a piece of data that can be
 -- reused in different instances of 'IsBox'. It has no 'BoxType' and hence
 -- defines no box.
-class IsBoxContent a where
+class IsBoxContent a  where
   boxSize :: a -> BoxSize
   boxBuilder :: a -> Builder
