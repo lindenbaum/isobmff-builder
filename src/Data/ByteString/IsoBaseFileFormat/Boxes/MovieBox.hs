f38daf6ef9ccf1a49b560dc23038324b75aafd0e
Sven Heyll <sven.heyll@lindenbaum.eu>
2016-07-20 11:15:13 +0200

    WIP Add Experimental Movie Header

--- src/Data/ByteString/IsoBaseFileFormat/Boxes/MovieBox.hs
+++ src/Data/ByteString/IsoBaseFileFormat/Boxes/MovieBox.hs
@@ -2,20 +2,14 @@
 module Data.ByteString.IsoBaseFileFormat.Boxes.MovieBox where
 
 import Data.ByteString.IsoBaseFileFormat.Boxes.Box
--- Uncomment this to see what the custom compiler errors look like:
---
-import Data.ByteString.IsoBaseFileFormat.Boxes.Skip
 
--- xxx :: Box "moov"
--- xxx = movieBox
---         (movieBoxContainer :- skipBox (Skip 100))
 -- | The metadata for a presentation, a single 'MovieBox' which occurs only once
 -- and top-level. It is pretty empty on it's own, but it contains nested boxes
 -- with all the relevant meta data.
 type MovieBox = Box "moov"
 
 instance BoxRules "moov" where
-  type RequiredNestedBoxes "moov" = '["mvhd", "trak"]
+  type RequiredNestedBoxes "moov" = '[MovieHeader, "trak"]
 
 -- | A movie parent box. Combine with the nested 'Boxes' using 'boxes' or '(^-)'
 --
@@ -30,3 +24,17 @@ instance BoxRules "moov" where
 --
 movieBox :: ParentBox "moov"
 movieBox = emptyParentBox
+
+-- | Overall media independent inforation, concerning the complete movie.
+data MovieHeader
+
+instance BoxRules MovieHeader where
+  type IsTopLevelBox MovieHeader = 'False
+  type GetCardinality MovieHeader any = 'ExactlyOnce
+
+data MovieHeaderFields time =
+  MovieHeaderFields {creationTime :: time
+                    ,modificationTime :: time
+                    ,timescale :: time
+                    ,duration :: time
+                    }
