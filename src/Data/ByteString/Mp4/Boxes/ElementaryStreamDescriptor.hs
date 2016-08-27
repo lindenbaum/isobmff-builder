ed8501f14f7ddcb7868c62eeca779e3c3f0904ff
Sven Heyll <sven.heyll@lindenbaum.eu>
2016-08-27 04:42:24 +0200

    WIP Add more to the ESDescr

--- src/Data/ByteString/Mp4/Boxes/ElementaryStreamDescriptor.hs
+++ src/Data/ByteString/Mp4/Boxes/ElementaryStreamDescriptor.hs
@@ -1,27 +1,35 @@
 {-# LANGUAGE UndecidableInstances #-}
-
 module Data.ByteString.Mp4.Boxes.ElementaryStreamDescriptor where
 
-import Data.ByteString.IsoBaseFileFormat.ReExports
-import Data.Type.BitRecords
-import Data.ByteString.Mp4.Boxes.BaseDescriptor
+import           Data.ByteString.IsoBaseFileFormat.ReExports
+import           Data.Type.BitRecords
+import           Data.ByteString.Mp4.Boxes.BaseDescriptor
 
--- * Decoder Specific Info
--- * Audio Object type -- TODO move into modules
-type AudioSpecificConfig audiConfig = ()
 
-type AudioObjectType n =
-  If (n <=? 30) (AudioObjectTypeSmall n) (AudioObjectTypeExt n)
 
-type AudioObjectTypeSmall n = "audioObjectType" :=> Field 5 := n
+type ESDescriptorAudioContent dependsOnEsId =
+                     "esId" :=> Word16
+  :>: (FlagJust dependsOnEsId)
+  :>: (FlagJust url)
+  :>: (FlagJust ocrEsId)
+  :>: "streamPriority"       :=> Field 5
+  :>: dependsOnEsId
+  :>: url
+  :>: ocrEsId
 
-type AudioObjectTypeExt n =
-  AudioObjectTypeSmall 31 :>: "audioObjectTypeExt" :=> Field 6 := (n - 32)
 
-type ESDescriptorAudioContent =
-  "ES_ID" :=> Word16 :>: "streamDependenceFlag" :=> Bool :>: 'False :>: "OCRstreamFlag" :=> Bool :>: "streamPriority" :=> Field 5
+-- * Interface from ISO 14496-3 (Audio)
+
+staticESDescriptorAudio :: StaticBaseDescriptorWithArgs (ES_DescrP ESDescriptorAudioContent)
+staticESDescriptorAudio =
+    staticBaseDescriptorWithArgs (ES_DescrP :: ES_DescrP ESDescriptorAudioContent)
 
 type StaticESDescriptorAudio = StaticBaseDescriptor (ES_DescrP ESDescriptorAudioContent)
 
-staticESDescriptorAudio :: StaticBaseDescriptorWithArgs (ES_DescrP ESDescriptorAudioContent)
-staticESDescriptorAudio = staticBaseDescriptorWithArgs (ES_DescrP :: ES_DescrP ESDescriptorAudioContent)
+type AudioSpecificConfig audiConfig = ()
+
+type AudioObjectType n = If (n <=? 30) (AudioObjectTypeSmall n) (AudioObjectTypeExt n)
+
+type AudioObjectTypeSmall n = "audioObjectType" :=> Field 5 := n
+
+type AudioObjectTypeExt n = AudioObjectTypeSmall 31 :>: "audioObjectTypeExt" :=> Field 6 := (n - 32)
